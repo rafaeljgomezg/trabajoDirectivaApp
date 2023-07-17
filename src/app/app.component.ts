188ae2eaf38c1e11d2da55c0f6fa0040200663bd
rafaeljgomezg <rafaeljgomezg@gmail.com>
2023-07-17 08:06:24 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trabajoDirectivaApp';
+  titulo="Registro de usuarios"
+  nombre=""
+  apellido=""
+  cargo=""
+
+  entradas=[
+    {titulo:'python cada dia mas presente'},
+    {titulo:'java presente desde hace mas de 20 años'},
+    {titulo:'JavaScript cada vez mas funcional'},
+    {titulo:'Codelink Potencia para tus APPs'},
+    {titulo:'¿Pascal, donde estas!!!!!?'}
+  ]
+
+  constructor(){
+   
+  }
+
+  mensaje=""
+  registrado=false
+
+  registrarUsuario(){
+    this.mensaje="Usuario registrado con exito"
+    this.registrado=true
+  }
 }
